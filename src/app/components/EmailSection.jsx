242d30e0bd804f0305a9ee83c26908b2f61caaa0
thiago <mavberg@hotmail.com>
2025-03-04 15:49:49 -0300

    changing colors

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -47,7 +47,7 @@ function sendEmail(e){
 
   return (
     <section className='grid md:grid-cols-2 my-12 md:my-12 py-12 py-24 gap-4 relative' id='contact'>
-      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2">
+      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2">
       </div>
       
       <div className='z-10'>
@@ -159,11 +159,9 @@ function sendEmail(e){
           </div>
           <button 
           type='submit'
-          className='bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 rounded-lg w-full'>
+          className='bg-blue-500 hover:bg-blue-600 text-white font-medium py-2.5 rounded-lg w-full'>
             Send Message
             </button>
-           
-           
            
            
           </form>
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
       <div className='grid grid-cols-1 sm:grid-cols-12'>
         <div className='col-span-7 place-self-center text-center sm:text-left'>
           <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
-           <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'> 
+           <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-900 via-purple-600 to-cyan-300 '> 
             Hello, I'm {""}
             </span>
             <br/>
@@ -38,7 +38,7 @@ const HeroSection = () => {
 
             <button 
             
-            className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>
+            className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-900 via-purple-600 to-cyan-300 hover:bg-slate-200 text-white'>
               
               <a href="https://api.whatsapp.com/send?phone=5585999037775" target='_blank'>
               Hit me a msn
@@ -46,7 +46,7 @@ const HeroSection = () => {
               </button>
             
             <button 
-                        className='px-1 py-1 w-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 sm:w-fit  rounded-full bg-transparent hover:bg-slate-800 text-white mt-3'>
+                        className='px-1 py-1 w-full bg-gradient-to-brfrom-blue-900 via-purple-600 to-cyan-300  sm:w-fit  rounded-full bg-transparent hover:bg-slate-800 text-white mt-3'>
              
               <a href='https://drive.google.com/file/d/135EARATCETQhQSjVo9tqtFJ4nZqexrYh/view' target="_blank"><span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2' a>Download CV</span></a>
               
